3efc7d36af9290cf391c1044261d24b7d2bdb643
Justabingereader <Harounimran04@gmail.com>
2023-10-21 09:39:46 +0100

    Portfolio Site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Body from './components/body';
+import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+import Dropdown from "./components/Dropdown";
 import './App.css';
+import "./index.css";
+import './toggle.css';
+import DarkMode from "./components/DarkMode";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+ 
+    return (
+      <BrowserRouter>
+        <div className="route-header">
+          <nav className="nav-head" style={{position: "fixed"}}>
+            <ul className="link-header" >
+            <div className="saver">
+            <DarkMode />
+            </div>
+              <li className="link-name">
+                <Link to="/">Imran Haroun</Link> 
+              </li>
+              <div className="dropdown">
+               <Dropdown  />
+              </div>
+              <li className="link-about">
+                <Link to="/about" >About</Link>
+              </li>
+              <li className="link-contact">
+                <Link to="/contact" >Contact</Link>
+              </li>
+              <li className="link-projects">
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+            
+          </nav>
+         
+        <Routes>
+          <Route path="/" end element={<Body/>} /> 
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 
 export default App;
