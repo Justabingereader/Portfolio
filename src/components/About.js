b03607c47c4d3d44bb723a5fc632fa0a11869cf4
Justabingereader <Harounimran04@gmail.com>
2023-10-23 16:38:00 +0100

    improved readability of content

--- src/components/About.js
+++ src/components/About.js
@@ -4,38 +4,36 @@ function About(){
     return (
         <div className="about-content">
             <h1 className="about-head">About Me</h1>
-            <p className= "about-body">Since i was young i have always been fascinated by the thought of
-                creating from the comfort of the computer, it always seemed like
-                the only path i would ever really consider for myself, and it was
-                no surprise to me when i indeed developed a passion for web
-                development while learning about it, the process of running into
-                a challenge and taking different steps to solve it interests me 
-                a lot and then the joy of seeing a working site and the feeling
-                of growth when you're done only brings satisfaction and it has 
-                only gotten better as i dive deeper into it.
-                I personally believe in continuing to acquire information and
-                knowledge from wherever i can from whichever field interests me
-                as i believe knowledge and information are some of the most
-                important things to have when navigating through life,
-                This is personally my 5th time of learning web development
-                but this is the deepest i have gone in all my attempts,
-                and i attribute that to the fact that it was something i
-                personally decided i would do hence i had enough drive to
-                stick with it even when it presented challenges. I am a newly
-                graduated Computer science student and am now on my way to becoming
-                a fully fledged web developer.
-                Since I finished school, I have devoted as much time as I can to
-                improve my web development skills and also my communication and
-                interpersonal skills as I believe these are important skills to have
-                in any workplace and you can never have too much of it in any environment.
-                Learning to code has also had positive effects in my personal life as it taught me to be 
-                accountable to myself and have better discipline with my time as I know I
-                have to put in hours everyday into learning web development no matter how
-                I may personally feel. And I am constantly working to better myself everyday
-                as i believe if you're not developing yourself whenever you can, then you
-                would be gradually left behind.  Some of the projects I've worked on can
-                be found on the projects page.
-</p>
+            <p className= "about-body">Since I was young, I have always been fascinated by the thought
+            of creating from the comfort of the computer. It always seemed like the only path I would
+            ever really consider for myself. It was no surprise to me when I indeed developed a passion
+            for web development while learning about it. The process of running into a challenge and
+            taking different steps to solve it interests me a lot. Then, the joy of seeing a working
+            site and the feeling of growth when you're done only brings satisfaction. It has only
+            gotten better as I dive deeper into it.<br/><br/>
+
+            I personally believe in continuing to acquire information and knowledge from wherever I can,
+            from whichever field interests me, as I believe knowledge and information are some of the most
+            important things to have when navigating through life.<br/><br/>
+
+            This is personally my 5th time of learning web development, but this is the deepest I have gone
+            in all my attempts. I attribute that to the fact that it was something I personally decided I
+            would do, hence I had enough drive to stick with it even when it presented challenges.
+            I am a newly graduated Computer Science student and am now on my way to becoming a fully-fledged
+            web developer.<br/><br/>
+
+            Since I finished school, I have devoted as much time as I can to improve my web development skills
+            and also my communication and interpersonal skills. I believe these are important skills to have in
+            any workplace, and you can never have too much of it in any environment.<br/><br/>
+
+            Learning to code has also had positive effects on my personal life as it taught me to be accountable
+            to myself and have better discipline with my time. I know I have to put in hours every day into
+            learning web development no matter how I may personally feel. I am constantly working to better
+            myself every day. I believe if you're not developing yourself whenever you can, then you would
+            be gradually left behind.<br/><br/>
+
+            Some of the projects I've worked on can be found on the projects page.
+        </p>
         </div>
     )
 };
--- src/components/body.js
+++ src/components/body.js
@@ -10,17 +10,16 @@ function Body(){
         <div className="header">
            <h4 className="head"> HTML, CSS, JS </h4>
            <h1 className="head2">Front End developer</h1>
-            <p className="body">Hello, Welcome to my Portfolio.
-                I am a front end web developer and my 
-                purpose for building this site was to 
-                have a central store for some of my projects,
-                Since i was young, i've always been captivated by 
-                the skill to make incredible things from the comfort of a computer.
-                I am currently learning Reactjs and that is the
-                library that was used in building this very site, although i can't 
-                claim that i understand all the intricacies of the library, i can claim to have a 
-                basic understanding of how it functions and while going through my learning process, i have
-                acquired a passion for building sites and pushing through the challenges they may present.
+            <p className="body">
+            Hello and welcome to my portfolio. I am a front-end web developer, and my purpose
+            for building this site was to have a central store for some of my projects.<br/><br/>
+
+            Since I was young, I've always been captivated by the skill to make incredible things
+            from the comfort of a computer. I am currently learning React.js, and that is the library
+            that was used in building this very site. Although I can't claim that I understand all the
+            intricacies of the library, I can claim to have a basic understanding of how it functions.
+            While going through my learning process, I have acquired a passion for building sites and
+            pushing through the challenges they may present.
             </p>
 
             <h2 className="discover">DISCOVER!</h2>
